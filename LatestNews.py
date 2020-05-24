0008a7c916840d1ae670e3a12dfc7c460089861b
FAROOQUE AHMED <zizo11@gmail.com>
2020-05-24 06:01:49 -0700

    Latest news

---
+++ LatestNews.py
@@ -0,0 +1,32 @@
+import urllib3
+from bs4 import BeautifulSoup
+
+
+def getLatestNews():
+    http = urllib3.PoolManager()
+    req = http.request('GET', 'https://www.dawn.com/latest-news')
+    print(req.status)
+    if req.status == 200:
+        soup = BeautifulSoup(req.data)
+        # print(soup.prettify())
+        news_article = soup.findAll('article', {'class': 'box'})
+
+        for news in news_article:
+            try:
+                news_parts = news.findAll('h2')
+                news_time_excerpt_parts = news.findAll('div', {'class': 'story__excerpt'})
+                for part in news_parts:
+                    news_head = part.text
+                    news_excerpt = news_time_excerpt_parts[0].text
+                    print(news_head)
+                    print(news_excerpt)
+
+                # print news.find('h2').text
+            except AttributeError:
+                continue
+    else:
+        print("Response was other than 200.")
+
+
+if __name__ == "__main__":
+     getLatestNews()
